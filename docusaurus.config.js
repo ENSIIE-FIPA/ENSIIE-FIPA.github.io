9c7256f6fc0d16876064e1cc33908d9156edf8ea
ENSIIE-FIPA <ensiiefipa@gmail.com>
2025-06-08 23:34:30 +0200

    Initialisation du site

---
+++ docusaurus.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'ENSIIE FIPA - Cours',
+  themeConfig: {
+    navbar: {
+      title: 'ENSIIE FIPA',
+      items: [
+        { to: '/docs/Semestre-1', label: 'Semestre 1', position: 'left' },
+        // ...
+      ],
+    },
+  },
+};
+
---
+++ sidebars.js
@@ -0,0 +1,25 @@
+module.exports = {
+  sidebar: [
+    {
+      type: 'category',
+      label: 'Semestre 1',
+      items: [
+        {
+          type: 'category',
+          label: 'Science de l\'Ingénieur',
+          items: [
+            'Semestre-1/SI/Matiere-1/cm',
+            'Semestre-1/SI/Matiere-1/td',
+            // ...
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Maths',
+          items: [/* ... */],
+        },
+      ],
+    },
+    // Répète pour Semestre 2, etc.
+  ],
+};
